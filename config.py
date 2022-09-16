adf3556324dfb7fc49a68e114c4f103840755282
Zelda Projects <56251778+nmiabdfhmy@users.noreply.github.com>
2022-09-16 16:00:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -25,25 +25,13 @@ BRANCH = getenv("BRANCH", "main")
 CHANNEL = getenv("CHANNEL", "ZeldaProjects")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN",
-    b64decode("Z2hwX1N3UTZHeDQ1NUJ1eVBHa01XNHk4M0hOb285MUZJUTBWMnhZeQ==").decode(
-        "utf-8"
-    ),
-)
+GIT_TOKEN = getenv("GIT_TOKEN")
 GROUP = getenv("GROUP", "HCMutualism")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 GCAST_BLACKLIST = {int(x) for x in os.environ.get("GCAST_BLACKLIST", "").split()}
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
-REPO_URL = getenv(
-    "REPO_URL",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL25taWFiZGZobXkvWmVsZGEtUHlyby1VYm90").decode("utf-8"),
-)
+REPO_URL = getenv("REPO_URL")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
-STRING_SESSION2 = getenv("STRING_SESSION2", "")
-STRING_SESSION3 = getenv("STRING_SESSION3", "")
-STRING_SESSION4 = getenv("STRING_SESSION4", "")
-STRING_SESSION5 = getenv("STRING_SESSION5", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
